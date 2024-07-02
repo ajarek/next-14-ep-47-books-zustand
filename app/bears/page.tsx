9106d3d6abe1a42dfaae0d04c6193e7c6145c2da
Ajarek <ajarek@poczta.onet.pl>
2024-07-03 00:21:21 +0200

    initial template

---
+++ app/bears/page.tsx
@@ -0,0 +1,21 @@
+'use client'
+import { useBearStore } from '@/store/bookStore'
+import Link from 'next/link'
+
+const Bears = () => {
+  const { increasePopulation, decreasePopulation, removeAllBears, bears } =
+    useBearStore()
+  return (
+    <div className='flex min-h-screen flex-col items-center justify-start gap-4 p-24'>
+      <Link
+        className='underline'
+        href='/'
+      >
+        Home
+      </Link>
+      <div className='text-2xl'>Bears {bears} 🐻</div>
+    </div>
+  )
+}
+
+export default Bears
